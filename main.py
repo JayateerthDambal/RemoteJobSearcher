7edf0b62bf0e0516321cf41497d088729a203bd1
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2023-10-12 22:56:50 +0530

    Added exporter endpoint

--- main.py
+++ main.py
@@ -32,5 +32,26 @@ def search():
 
     return render_template("job_search.html", input=input, jobs=jobs, job_count=len(jobs))
 
+
+@app.route("/export")
+def export():
+    try:
+        input = request.args.get("input")
+
+        if not input:
+            raise Exception()
+
+        input = input.lower()
+        remote_jobs = db.get(input)
+
+        if not remote_jobs:
+            raise Exception()
+
+        save_to_file(remote_jobs)
+
+        return send_file("remote_jobs.csv")
+    except:
+        return redirect("/")
+
 app.run(host="127.0.0.1")
 
